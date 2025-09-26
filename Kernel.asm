a811741e47e31869e7a09b370f7656c089b38797
Khorlane <khorlane@yahoo.com>
2025-09-26 11:18:22 -0400

    Just moving stuff around. No functional changes.

--- Kernel.asm
+++ Kernel.asm
@@ -18,9 +18,7 @@
 ; Kernel Entry Point
 ;--------------------------------------------------------------------------------------------------
 Stage3:
-  ;--------------
   ; Set registers
-  ;--------------
  	mov		ax,0x10
   mov   ds,ax                         ;  segments to
   mov   ss,ax                         ;  data selector
@@ -29,80 +27,25 @@ Stage3:
   ; Load the GDT
   lea   eax, [GDTDescriptor]          ; Load the address of GDTDescriptor into EAX
   lgdt  [eax]                         ; Load the GDT using the address in EAX
-  
+  ; Start it up
   call BootMsg                        ; Print boot message
-
-  ;------------------------
-  ; Initialize the 8259 PIC
-  ;------------------------
-  mov   al,00010001b                  ; Set ICW1
-  out   PIC1_CTRL,al                  ;  Intialize
-  out   PIC2_CTRL,al                  ;  8259
-  mov   al,020h                       ; Set ICW2
-  out   PIC1_DATA,al                  ;  Map
-  mov   al,028h                       ;  IRQs
-  out   PIC2_DATA,al                  ;  32-47
-  mov   al,00000100b                  ; Set ICW3
-  out   PIC1_DATA,al                  ;  Connect PIC1
-  mov   al,00000010b                  ;  and PIC2
-  out   PIC2_DATA,al                  ;  via IRQ line 2
-  mov   al,00000001b                  ; Set ICW4
-  out   PIC1_DATA,al                  ;  We are in
-  out   PIC2_DATA,al                  ;  80x86 mode
-  call  SetPIT                        ; Configure the PIT for timer interrupts
-  mov   ebx,Msg7                      ; Put
-  call  DebugIt                       ;  Msg7
-
-  ;--------------
-  ; Set Timer IDT
-  ;--------------
-  mov   edx,020h                      ; Timer IRQ 0 is now IRQ 32 (020h)
-  shl   edx,3                         ; Position into
-  add   edx,IDT                       ;  the IDT
-  mov   ax,08E00h                     ; Stash
-  mov   [EDX+4],ax                    ;  stuff
-  mov   eax,IsrTimer                  ;  into
-  mov   [edx],ax                      ;  the IDT
-  shr   eax,16                        ;  to link IRQ 32
-  mov   [EDX+6],ax                    ;  to the
-  mov   ax,008h                       ;  correct ISR
-  mov   [EDX+2],ax                    ;  which is IsrTimer
-  mov   ebx,Msg8                      ; Put
-  call  DebugIt                       ;  Msg8
-
-  ;-----------------
-  ; Set Keyboard IDT  
-  ;-----------------
-  mov   edx,021h                      ; IRQ1 maps to vector 0x21  
-  shl   edx,3                         ; Multiply by 8 (IDT entry size)
-  add   edx,IDT                       ; Point to correct IDT slot
-  mov   ax,08E00h                     ; Present, DPL=0, 32-bit interrupt gate
-  mov   [EDX+4],ax                    ; Set access rights
-  mov   eax,IsrKeyboard               ; Address of ISR
-  mov   [edx],eax                     ; Low 16 bits of offset  
-  shr   eax,16                        ; High 16 bits of offset  
-  mov   [EDX+6],eax                   ; Set high offset  
-  mov   ax,008h                       ; Code segment selector  
-  mov   [EDX+2],ax                    ; Set segment selector
-  mov   ebx,Msg9                      ; Put
-  call  DebugIt                       ;  Msg9
+  call InitPic                        ; Initialize PIC
+  call InitPit                        ; Initialize PIT
+  call SetTimerIdt                    ; Set Timer IDT
+  call SetKeyboardIdt                 ; Set Keyboard IDT
 
 .halt:
   jmp .halt                           ; Infinite loop to prevent return
 
 BootMsg:  
-  ;-------------
-  ; Clear screen
-  ;-------------
+  ; Set colors and clear screen
   mov   al,Black                      ; Background
   mov   [ColorBack],al                ;  color
   mov   al,Purple                     ; Foreground
   mov   [ColorFore],al                ;  color
   call  SetColorAttr                  ; Set color
   call  ClrScr                        ; Clear screen
-  ;--------------
   ; Print success
-  ;--------------
   mov   al,10                         ; Set
   mov   [Row],al                      ;  Row,Col
   mov   al,1                          ;  to
@@ -116,27 +59,79 @@ BootMsg:
   mov   ebx,NewLine                   ; Put
   call  PutStr                        ;  a New Line
   ret
+    
+InitPic:
+  mov   al,00010001b                  ; Set ICW1
+  out   PIC1_CTRL,al                  ;  Initialize
+  out   PIC2_CTRL,al                  ;  8259 PIC
+  mov   al,020h                       ; Set ICW2
+  out   PIC1_DATA,al                  ;  Map
+  mov   al,028h                       ;  IRQs
+  out   PIC2_DATA,al                  ;  32-47
+  mov   al,00000100b                  ; Set ICW3
+  out   PIC1_DATA,al                  ;  Connect PIC1
+  mov   al,00000010b                  ;  and PIC2
+  out   PIC2_DATA,al                  ;  via IRQ line 2
+  mov   al,00000001b                  ; Set ICW4
+  out   PIC1_DATA,al                  ;  We are in
+  out   PIC2_DATA,al                  ;  80x86 mode
+  mov   ebx,Msg3                      ; Put
+  call  DebugIt                       ;  Msg3
+  ret
+
+InitPit:
+  ; Configure PIT for 18.2 Hz (default frequency)
+  mov   al,00110110b                  ; Set PIT to Mode 3 (Square Wave Generator)
+  out   43h,al                        ; Write to PIT control port
+  mov   ax,0FFFFh                     ; Divisor for 18.2 Hz (default)
+  out   40h,al                        ; Write low byte of divisor
+  mov   al,ah                         ; Write high
+  out   40h,al                        ;  byte of divisor
+  mov   ebx,Msg4                      ; Put
+  call  DebugIt                       ;  Msg4
+  ret
+
+SetTimerIdt:
+  mov   edx,020h                      ; Timer IRQ 0 is now IRQ 32 (020h)
+  shl   edx,3                         ; Position into
+  add   edx,IDT                       ;  the IDT
+  mov   ax,08E00h                     ; Stash
+  mov   [EDX+4],ax                    ;  stuff
+  mov   eax,IsrTimer                  ;  into
+  mov   [edx],ax                      ;  the IDT
+  shr   eax,16                        ;  to link IRQ 32
+  mov   [EDX+6],ax                    ;  to the
+  mov   ax,008h                       ;  correct ISR
+  mov   [EDX+2],ax                    ;  which is IsrTimer
+  mov   ebx,Msg5                      ; Put
+  call  DebugIt                       ;  Msg5
+  ret
+
+SetKeyboardIdt:
+  mov   edx,021h                      ; IRQ1 maps to vector 0x21  
+  shl   edx,3                         ; Multiply by 8 (IDT entry size)
+  add   edx,IDT                       ; Point to correct IDT slot
+  mov   ax,08E00h                     ; Present, DPL=0, 32-bit interrupt gate
+  mov   [EDX+4],ax                    ; Set access rights
+  mov   eax,IsrKeyboard               ; Address of ISR
+  mov   [edx],eax                     ; Low 16 bits of offset  
+  shr   eax,16                        ; High 16 bits of offset  
+  mov   [EDX+6],eax                   ; Set high offset  
+  mov   ax,008h                       ; Code segment selector  
+  mov   [EDX+2],ax                    ; Set segment selector
+  mov   ebx,Msg6                      ; Put
+  call  DebugIt                       ;  Msg6
+  ret
 
 DebugIt:
   call PutStr                         ; Print string at EBX
   mov   ebx,NewLine                   ; Put
   call  PutStr                        ;  a New Line
-    
-;----------------------------------------------
-; Configure PIT for 18.2 Hz (default frequency)
-;----------------------------------------------
-SetPIT:
-  mov   al,00110110b                  ; Set PIT to Mode 3 (Square Wave Generator)
-  out   43h,al                        ; Write to PIT control port
-  mov   ax,0FFFFh                     ; Divisor for 18.2 Hz (default)
-  out   40h,al                        ; Write low byte of divisor
-  mov   al,ah                         ; Write high byte of divisor
-  out   40h,al
   ret
 
-;----------
-; ISR Timer
-;----------
+;--------------------------------------------------------------------------------------------------
+; Interrupt Service Routines (ISRs)
+;--------------------------------------------------------------------------------------------------
 IsrTimer:
   pushad
   inc   dword [TimerTicks]            ; Increment the tick counter
@@ -145,9 +140,6 @@ IsrTimer:
   popad
   iretd
 
-;-------------
-; ISR Keyboard  
-;-------------
 IsrKeyboard:  
   pushad  
   in al,060h                         ; Read scancode from keyboard  
@@ -167,20 +159,17 @@ IsrKeyboard:
 %endmacro
 String  Msg1,"------   AsmOSx86 v0.0.2   -----"
 String  Msg2,"--------  32 Bit Kernel --------"
-String  Msg3,"AsmOSx86 has ended!!"
-String  Msg4,"ISR Timer Started"
-String  Msg5,"Start clearing keyboard buffer"
-String  Msg6,"Finished clearing keyboard buffer"
-String  Msg7,"PIT Configured"
-String  Msg8,"Timer IDT Set"
-String  Msg9,"Keyboard IDT Set"
-String  FaultMsg,"------   FAULT: System Halted   ------"
+String  Msg3,"Init PIC"
+String  Msg4,"Init PIT"
+String  Msg5,"Set IDT - Timer"
+String  Msg6,"Set IDT - Keyboard"
 String  NewLine,0Ah
 String  Buffer,"XXXXXXXX"
-
-String  DivideMsg, "Divide by zero fault"
-String  DoubleMsg, "Double fault triggered"
-
+String  MsgA,"AsmOSx86 has ended!!"
+String  MsgB,"ISR Timer Started"
+String  MsgC,"Start clearing keyboard buffer"
+String  MsgD,"Finished clearing keyboard buffer"
+String  MsgE,"Keyboard IDT Set"
 
 ColorBack   db  0                       ; Background color (00h - 0Fh)
 ColorFore   db  0                       ; Foreground color (00h - 0Fh)
@@ -227,19 +216,18 @@ align 4
 IDT:
 IDT1: times 2048 db 0
 IDT2:
-    dw 2047
-    dd IDT1
+  dw 2047
+  dd IDT1
 
 ;--------------------------------------------------------------------------------------------------
 ; Global Descriptor Table (GDT) for Kernel
 ;--------------------------------------------------------------------------------------------------
 align 8
 GDTTable:
-    dq 0x0000000000000000       ; Null descriptor (selector 0x00)
-    dq 0x00CF9A000000FFFF       ; Code segment (selector 0x08)
-    dq 0x00CF92000000FFFF       ; Data segment (selector 0x10)
+  dq 0x0000000000000000       ; Null descriptor (selector 0x00)
+  dq 0x00CF9A000000FFFF       ; Code segment (selector 0x08)
+  dq 0x00CF92000000FFFF       ; Data segment (selector 0x10)
 GDTTableEnd:
-
 GDTDescriptor:
-    dw GDTTableEnd - GDTTable - 1   ; Limit = size of GDT - 1 (should be 23 bytes)
-    dd 0x00100808                   ; Base = linear address of GDTTable (1MB + offset)
+  dw GDTTableEnd - GDTTable - 1   ; Limit = size of GDT - 1 (should be 23 bytes)
+  dd 0x00100808                   ; Base = linear address of GDTTable (1MB + offset)
