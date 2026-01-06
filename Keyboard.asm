2a6564f3a74292b35d5e87e0aa9ea88396921db8
Khorlane <khorlane@yahoo.com>
2026-01-06 16:27:31 -0500

    Cosmetic - Keyboard.asm

--- Keyboard.asm
+++ Keyboard.asm
@@ -11,19 +11,19 @@
 
 ; Read scancode
 KbRead:
-    mov   ecx,2FFFFh                    ; Set count for loop
+  mov   ecx,2FFFFh                      ; Set count for loop
 KbWait:
-    in    al,064h                       ; Read 8042 Status Register (bit 1 is input buffer status (0=empty, 1=full)
-    test  al,1                          ; If bit 1
-    jnz   KbGetIt                       ;  go get scancode
-    loop  KbWait                        ; Keep looping
-    mov   al,0FFh                       ; No scan
-    mov   [KbChar],al                   ;  code received
-    ret                                 ; All done!
+  in    al,064h                         ; Read 8042 Status Register (bit 1 is input buffer status (0=empty, 1=full)
+  test  al,1                            ; If bit 1
+  jnz   KbGetIt                         ;  go get scancode
+  loop  KbWait                          ; Keep looping
+  mov   al,0FFh                         ; No scan
+  mov   [KbChar],al                     ;  code received
+  ret                                   ; All done!
 KbGetIt:
-    in    al,060h                       ; Obtain scancode from
-    mov   [KbChar],al                   ;  Keyboard I/O Port
-    ret                                 ; All done!
+  in    al,060h                         ; Obtain scancode from
+  mov   [KbChar],al                     ;  Keyboard I/O Port
+  ret                                   ; All done!
 
 ; Translate scancode to ASCII character
 KbXlate:
