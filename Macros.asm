737f2864b741379f043db80f7f431e995b48f0ec
Khorlane <khorlane@yahoo.com>
2026-01-19 16:11:57 -0500

    Make it clear that Str and String are the exact same thing

--- Macros.asm
+++ Macros.asm
@@ -1,4 +1,5 @@
-; String Macro - Define a string with length prefix
+; Str (aka String) = [u16 len][bytes...]
+; The ONLY string representation in the kernel.
 %macro String 2+
 %1          dw  %%EndStr-%1-2
             db  %2
